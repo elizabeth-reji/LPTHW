5bc0f804c51e053c872cceeebd07013d3fa858eb
Elizabeth Reji <elizabethreji123@gmail.com>
2021-09-04 16:57:40 +0530

    stack completed

--- stack.py
+++ stack.py
@@ -1,25 +1,20 @@
 stack = []
+
 while True:
     choice = input("> ")
     if choice == "exit":
         exit()
-
     choice = choice.split()
-    #print(choice)
     if choice[0] == "push":
         stack.insert(0,choice[1])
     elif choice[0] == "show":
-        print(stack)
-
-
-
-
-
-
-
-
-#while <condition>:
-#    statement 1
-#    statment 2
-#    print(adada)
-#    input(adad)
+        for i in range(len(stack)):
+            print(f"| {stack[i]} |")
+        print("-----")
+    elif choice[0] == "pop":
+        print(stack.pop(0))
+        if len(stack) == 0:
+            print("No more numbers to pop")
+            exit()
+    else:
+        print("invalid inputs")
