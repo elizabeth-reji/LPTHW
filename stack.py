57f39de895ee4f9350d891608e42c3e5f85ea14b
Elizabeth Reji <elizabethreji123@gmail.com>
2021-09-07 06:50:10 +0530

    stack completed finally

--- stack.py
+++ stack.py
@@ -4,17 +4,25 @@ while True:
     choice = input("> ")
     if choice == "exit":
         exit()
+
     choice = choice.split()
+
     if choice[0] == "push":
-        stack.insert(0,choice[1])
+        if len(choice) != 2:
+            print("invaild input")
+        else:
+            stack.insert(0,choice[1])
+
     elif choice[0] == "show":
         for i in range(len(stack)):
             print(f"| {stack[i]} |")
         print("-----")
+
     elif choice[0] == "pop":
-        print(stack.pop(0))
+
         if len(stack) == 0:
             print("No more numbers to pop")
-            exit()
+        else:
+            print(stack.pop(0))
     else:
         print("invalid inputs")
